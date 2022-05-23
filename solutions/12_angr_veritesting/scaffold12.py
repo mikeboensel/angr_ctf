b4ad4624ca387e15d6e777a44d3b2dd75dfdb2e8
Michael Boensel <michael.boensel@ll.mit.edu>
2022-05-23 12:00:20 -0400

    Solution

--- solutions/12_angr_veritesting/scaffold12.py
+++ solutions/12_angr_veritesting/scaffold12.py
@@ -13,7 +13,7 @@ def main(argv):
     #Left off FILL UNCONSTRAINED options. Never 100% clear on whether those were needed.
     # Still works
     initial_state = proj.factory.entry_state()
-
+    #TODO: What is veritest doing for us? W/ it takes 15seconds. W/o the fans just spin and no progress is made. 
     sim = proj.factory.simgr(initial_state, veritesting=True)
 
     sim.explore(find=0x08048693, avoid=0x0804869)
